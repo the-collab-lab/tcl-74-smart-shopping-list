656e62c1c70bcd56599c964e366c9629ebc52dbe
Stacy Daniel <70743760+stacy-tech@users.noreply.github.com>
2024-08-19 14:05:00 -0400

    update form with time realated choices and add label element

--- src/views/ManageList.jsx
+++ src/views/ManageList.jsx
@@ -3,6 +3,7 @@ import { addItem } from '../api/firebase';
 
 export function ManageList() {
 	const [itemName, setItemName] = useState('');
+	const [daysUntilNextPurchase, setDaysUntilNextPurchase] = useState(7);
 
 	return (
 		<div>
@@ -15,6 +16,36 @@ export function ManageList() {
 				onChange={(e) => setItemName(e.target.value)}
 				required
 			/>
+			<fieldset>
+				<legend>How soon will you need to buy this item again?</legend>
+				<label>
+					<input
+						type="radio"
+						value={7}
+						checked={daysUntilNextPurchase === 7}
+						onChange={() => setDaysUntilNextPurchase(7)}
+					/>
+					Soon (7 days)
+				</label>
+				<label>
+					<input
+						type="radio"
+						value={14}
+						checked={daysUntilNextPurchase === 14}
+						onChange={() => setDaysUntilNextPurchase(14)}
+					/>
+					Kind of soon (14 days)
+				</label>
+				<label>
+					<input
+						type="radio"
+						value={30}
+						checked={daysUntilNextPurchase === 30}
+						onChange={() => setDaysUntilNextPurchase(30)}
+					/>
+					Not soon (30 days)
+				</label>
+			</fieldset>
 		</div>
 	);
 }
