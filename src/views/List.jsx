c7c5826ae182c5e942204e03c39f0898096498cf
NickRoccodev11 <nchls.rcc@gmail.com>
2024-08-26 13:41:51 -0400

    Add input and state for search functionality
     2 of 3, button conditionally renders based on state

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -2,11 +2,33 @@ import { useState } from 'react';
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	const [searchInput, setSearchInput] = useState('');
+
+	const handleInputChange = (e) => {
+		setSearchInput(e.target.value);
+	};
+
+	const clearSearchInput = () => {
+		setSearchInput('');
+	};
+
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
+			<div className="listSearch">
+				<label htmlFor="search"> Search Items</label>
+				<br />
+				<input
+					type="text"
+					id="search"
+					value={searchInput}
+					onChange={handleInputChange}
+					placeholder="Type to search..."
+				/>
+				{searchInput && <button onClick={clearSearchInput}>X</button>}
+			</div>
 			<ul>
 				{/**
 				 * uses ternary operator to display "no items" msg if list is empty
