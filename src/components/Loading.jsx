e04d2179ca189a1b2b5d157c078681a086381dc2
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-10-07 17:22:14 -0400

    add tailwind to Loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,12 +1,16 @@
+import logo from '../assets/logo.png';
+
 export function Loading() {
 	return (
-		<div style={{ textAlign: 'center', padding: '20px' }}>
+		<div className="flex flex-col items-center justify-center min-h-screen p-4 text-center bg-[#99D98C]">
 			<img
-				src={`${import.meta.env.BASE_URL}logo.png`}
-				alt="Loading..."
-				style={{ width: '150px' }}
+				src={logo}
+				alt="CollabShop Logo, a collaboration platform for grocery shopping."
+				className="w-24 sm:w-32 md:w-40 h-auto mb-4"
 			/>
-			<h2>Please hold on while we prepare your experience.</h2>
+			<h2 className="text-[#184E77] text-base sm:text-lg md:text-xl font-semibold">
+				Please hold on while we prepare your experience.
+			</h2>
 		</div>
 	);
 }
