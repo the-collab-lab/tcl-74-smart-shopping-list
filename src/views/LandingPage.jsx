1badbfd315863a780661ac6a7a0ac619ea91ea4d
Stacy Daniel <70743760+stacy-tech@users.noreply.github.com>
2024-10-09 23:15:58 -0400

    Modified the landing page to ensure that it uses the dark/light mode theme using the appropriate background and text color classes

--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -2,13 +2,13 @@ import logo from '../assets/logo.png';
 
 export function LandingPage() {
 	return (
-		<div className="flex flex-col items-center justify-center min-h-screen bg-bgPrimary p-6">
+		<div className="flex flex-col items-center justify-center min-h-screen bg-bgPrimary dark:bg-bgPrimaryDark p-6">
 			<img
 				src={logo}
 				alt="CollabShop Logo, a collaboration platform for grocery shopping."
 				className="w-45 h-auto mb-6"
 			/>
-			<h1 className="text-txtPrimary text-2xl md:text-4xl font-bold text-center mb-4">
+			<h1 className="text-txtPrimary dark:text-txtPrimaryDark text-2xl md:text-4xl font-bold text-center mb-4">
 				<strong>CollabShop</strong> is more than just a grocery app—it&apos;s a
 				tool that embodies the spirit of teamwork and collaboration. Created by
 				early-career developers from{' '}
@@ -16,7 +16,7 @@ export function LandingPage() {
 					href="https://the-collab-lab.codes/"
 					target="_blank"
 					rel="noopener noreferrer"
-					className="text-txtPrimary font-bold underline hover:text-[#1A759F] transition duration-300"
+					className="text-txtPrimary dark:text-txtPrimaryDark font-bold underline hover:text-[#1A759F] transition duration-300"
 				>
 					<strong>The Collab Lab</strong>
 				</a>
@@ -25,7 +25,7 @@ export function LandingPage() {
 				effortlessly share and collaborate on lists. With CollabShop, planning
 				your grocery trips becomes a seamless and enjoyable experience!
 			</h1>
-			<p className="text-txtPrimary font-bold text-lg md:text-xl text-center mb-6">
+			<p className="text-txtPrimary dark:text-txtPrimaryDark font-bold text-lg md:text-xl text-center mb-6">
 				Ready to start your journey? Click the sign-in button below to begin
 				planning your grocery runs with CollabShop today.
 			</p>
