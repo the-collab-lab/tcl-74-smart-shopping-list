6f039e540eefa5053b49a9f82038dc5403796e73
Stacy Daniel <70743760+stacy-tech@users.noreply.github.com>
2024-10-09 23:04:57 -0400

    updated the tailwind file to define custom colors for light and dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,jsx}'],
+	darkMode: 'class', // Enable dark mode using the class strategy
 	theme: {
 		extend: {
 			colors: {
@@ -9,6 +10,11 @@ export default {
 				bgSecondary: '#34A0A4', // Accent color for NavBar
 				txtPrimary: '#184E77', // Primary color Text
 				txtSecondary: '#34A0A4', // Secondary color Text
+				//Dark mode colors
+				bgPrimaryDark: '#1D1D1D', // Dark background color
+				bgSecondaryDark: '#2C2C2C', // Dark secondary color
+				txtPrimaryDark: '#FFFFFF', // Light text color
+				txtSecondaryDark: '#A0A0A0', // Secondary light text color
 			},
 		},
 	},
