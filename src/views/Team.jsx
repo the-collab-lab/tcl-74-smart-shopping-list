312858359f1715f5fd1e5270841eae813b891a8c
DoribelTerceroParker <dtercerocunystart@gmail.com>
2024-10-13 19:40:35 -0400

    change Team tailwind grid to 2 for developers

--- src/views/Team.jsx
+++ src/views/Team.jsx
@@ -102,7 +102,7 @@ export function Team() {
 					<span className="text-lg mt-2">View CollabShop on GitHub</span>
 				</a>
 			</div>
-			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
+			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8 justify-items-center">
 				{developers.map((dev, index) => (
 					<div
 						key={index}
